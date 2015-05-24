b20ef934d4f1644f17acc409e587799c3de21180
Luis Fernando <lfmexi@gmail.com>
2015-05-24 17:19:31 -0600

    modificado: services/login.php

--- services/login.php
+++ services/login.php
@@ -3,25 +3,31 @@ $ipdb = '127.0.0.1:3306';
 $userdb = 'root';
 $passwd = 'tesisHealthyFood';
 $dbname = 'healthyFood';
+
 header('Content-Type: text/html; charset=utf-8');
-$conn = mysql_connect($ipdb,$userdb,$passwd);
-mysql_selectdb($dbname, $conn);
-if($_POST){
-	$user=$_POST['user'];
-	$pass=md5($_POST['pass']);
-	$query = "SELECT id,nick,nombre,birth, sexo as sex FROM Usuario WHERE nick='".$user."' and pass='".$pass."';";
-	//mysql_query("BEGIN");
-	$result1 = mysql_query($query);
-	//mysql_query("COMMIT");
-	if($result1){
-        $bdds = array();
-	    $campo=array_keys($bdds);
-	
-	    while($r = mysql_fetch_assoc($result1)){
-		    $bdds[] = $r;
+
+$conn = new mysqli($ipdb,$userdb,$passwd,$dbname);
+
+if($conn){
+	if($_POST){
+		$user=$_POST['user'];
+		$pass=md5($_POST['pass']);
+		$query = "SELECT id,nick,nombre,birth, sexo as sex FROM Usuario WHERE nick='$user' and pass='$pass';";
+		//mysql_query("BEGIN");
+		$result1 = $conn->query($query);
+		//mysql_query("COMMIT");
+		if($result1){
+
+	        $bdds = array();
+		    $campo=array_keys($bdds);
+		
+		    while($r = $result1->fetch_assoc()){
+			    $bdds[] = $r;
+		    }
+		    echo json_encode($bdds);
 	    }
-	    mysql_close($conn);
-	    echo json_encode($bdds);
-    }
+	    $result1->close();
+	}	
 }
+$conn->close();
 ?>
