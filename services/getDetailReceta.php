c3cf7549d05080ae3ab468b8bee23161111e19db
Luis Fernando <lfmexi@gmail.com>
2015-05-24 21:57:21 -0600

    modificado: getDetailReceta.php

--- services/getDetailReceta.php
+++ services/getDetailReceta.php
@@ -11,8 +11,7 @@ if($_REQUEST){
         $query= "SELECT u.nick AS username, r.Nombre AS nombre, r.Categoria AS categoria, SUM( i.cantidad*ri.gramos/100 ) AS calorias, r.Instrucciones AS instrucciones, r.Foto AS url ";
         $query= $query . "FROM Usuario u JOIN Receta AS r ON r.Usuario=u.id JOIN Receta_Ingrediente AS ri ON r.idReceta = ri.idReceta JOIN Ingrediente AS i ON ri.idIngrediente = i.id ";
         $query= $query . "WHERE r.Nombre = '".$recipe."'";
-    echo $query;
-	//mysql_query("BEGIN");
+    //mysql_query("BEGIN");
 	$result1 = mysql_query($query);
 	//mysql_query("COMMIT");
 	if($result1){
